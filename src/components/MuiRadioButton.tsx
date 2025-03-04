0b98cf83cd6722fa53110ee48e3b18a8e32bfdbd
Mayur Paunipagar <mayur.paunipagar@homeserveusa.com>
2025-03-04 19:47:01 +0530

    radio button group complete

--- src/components/MuiRadioButton.tsx
+++ src/components/MuiRadioButton.tsx
@@ -8,5 +8,20 @@ import {
 } from "@mui/material"
 
 export const MuiRadioButton = () => {
-    return <Box>MuiRadioButton</Box>
+    return <Box>
+        <FormControl>
+            <FormLabel id="years-of-experience">
+                Years Of Experience
+            </FormLabel>
+            <RadioGroup 
+                name="job-experience-group"
+                aria-labelledby="job-experience-group-label"
+
+            >
+                <FormControlLabel control={<Radio/>} label='0-2' value='0-2'></FormControlLabel>
+                <FormControlLabel control={<Radio/>} label='3-5' value='3-5'></FormControlLabel>
+                <FormControlLabel control={<Radio/>} label='6-10' value='6-10'></FormControlLabel>
+            </RadioGroup>
+        </FormControl>
+    </Box>
 }
