fc44cd77f92b6f0a4e42400751bcce8d9dcc3886
Mayur Paunipagar <mayur.paunipagar@homeserveusa.com>
2025-03-04 10:18:56 +0530

    added states on error

--- src/components/MuiTextField.tsx
+++ src/components/MuiTextField.tsx
@@ -1,8 +1,22 @@
 import { InputAdornment, Stack, TextField } from "@mui/material"
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import { useState } from "react";
 
 export const MuiTextField = () => {
+    const [value, setValue] = useState('')
     return (
         <Stack spacing={4}>
+            <Stack direction={"row"} spacing={2}>
+                <TextField
+                    label="password"
+                    type="password"
+                    helperText="Do not share your password with anyone"
+                    disabled
+                    InputProps={{
+                        endAdornment: <VisibilityOffIcon></VisibilityOffIcon>
+                    }}
+                />
+            </Stack>
             <Stack direction={"row"} spacing={2}>
 
                 {/* <TextField label="name"/> //default is outlined */}
@@ -18,11 +32,44 @@ export const MuiTextField = () => {
                 <TextField label="small secondary" color="secondary" size="small" />
             </Stack>
             <Stack direction={"row"} spacing={2}>
-                <TextField label="form input " color="secondary" size="small" required />
+                <TextField
+                    label="form input "
+                    color="secondary"
+                    size="small"
+                    required
+                    value={value}
+                    onChange={(e) => setValue(e.target.value)}
+                    error={!value}
+                    helperText={!value?"Required":"Do not share"} />
             </Stack>
             <Stack direction={"row"} spacing={2}>
-                <TextField label="password" type="password" helperText="Do not share your password with anyone" disabled/>
-                <TextField label="Read Only" InputProps={{readOnly:true}}/>
+                <TextField
+                    label="password"
+                    type="password"
+                    helperText="Do not share your password with anyone"
+                    disabled
+                    InputProps={{
+                        endAdornment: <VisibilityOffIcon></VisibilityOffIcon>
+                    }} />
+            </Stack>
+            <Stack direction={"row"} spacing={2}>
+                <TextField
+                    label="password"
+                    type="password"
+                    helperText="Do not share your password with anyone"
+                    disabled
+                />
+            </Stack>
+            <Stack direction={"row"} spacing={2}>
+                <TextField
+                    label="password"
+                    type="password"
+                    helperText="Do not share your password with anyone" />
+
+
+
+
+                <TextField label="Read Only" InputProps={{ readOnly: true }} />
             </Stack>
             <Stack direction={"row"} spacing={2}>
                 <TextField label="Amount" InputProps={{
@@ -31,11 +78,11 @@ export const MuiTextField = () => {
 
                 </TextField>
                 <TextField label="Weight" InputProps={{
-                    endAdornment:<InputAdornment position="end">KG</InputAdornment>
+                    endAdornment: <InputAdornment position="end">KG</InputAdornment>
                 }}>
 
                 </TextField>
-                
+
 
             </Stack>
 
